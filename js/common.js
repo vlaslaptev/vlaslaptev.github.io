46033f129a269c2f6fe42958c688bce3ac71771a
Vlas Laptev <vlaptev@vp.ru>
2016-10-01 17:16:54 +0600

    add href

--- js/common.js
+++ js/common.js
@@ -1,5 +1,9 @@
-$(".nav a").on("click", function(){
-		$(".nav").find(".active").removeClass("active");
-		$(this).parent().addClass("active");
+$(".nav a").on("click", function(e){
+	e.preventDefault();
+	e.stopPropagation();
+	$(".nav").find(".active").removeClass("active");
+	$(this).parent().addClass("active");
+
+	$(".content").load($(this).href);
 });
 
